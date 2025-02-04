328e48edc7760f7bf4c91b14c4dd98332a6376a8
Brent Theisen <brent@bantamlabs.com>
2025-02-04 12:21:31 -0600

    MG JV config

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import { Scoreboard } from './Scoreboard';
 
 const props = {
   visitor: {
-    teamName: "Waupun",
-    color: "#E4B548",
-    backgroundColor: "#000000",
+    teamName: "MG",
+    color: "#91B2D3",
+    backgroundColor: "#284185",
   },
   home: {
-    teamName: "Navs",
+    teamName: "Navs JV",
     color: "#1E2A48",
     backgroundColor: "#EEB650",
   },
   clock: {
-    minutes: 17,
+    minutes: 15,
     color: "#ffffff",
     backgroundColor: "#333333",
   },
